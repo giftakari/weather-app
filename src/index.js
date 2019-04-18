0744758499d3caf8ff3026f9e0cd402537c97c67
Godsgift Akari <giftakari@yahoo.com>
2019-04-19 08:26:47 +1000

    Added  Components

--- src/index.js
+++ src/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 import './index.css';
-import App from './App';
+
+
+
+class App extends React.Component{
+
+  state = {lat:null, errorMessage:''};
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat:position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
+      
+    );
+  }
+
+
+ 
+  
+
+  render(){
+    if(this.state.errorMessage && !this.state.lat){
+      return <div><h1>Error:{this.state.errorMessage}</h1></div> 
+    }
+    if(!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay />
+    }    
+  return <div className='loader'> </div>
+  }
+
+}
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
